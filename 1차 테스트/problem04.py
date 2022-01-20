54e5f72cff1b0704db3322c599b43b2bd8c8d360
talksame <rnjsdndud4@gmail.com>
2022-01-20 16:51:00 +0900

    0120_2

---
+++ 1차 테스트/problem04.py
@@ -0,0 +1,31 @@
+import json
+
+
+def turn(temperatures):
+    dict_temp = {
+        'maximum' : [],
+        'minimum' : []
+    }
+    for i in temperatures:
+        dict_temp['maximum'].append(i[0])
+        dict_temp['minimum'].append(i[1])
+    return dict_temp
+    # 여기에 코드를 작성합니다.
+
+
+# 아래의 코드는 수정하지 않습니다.
+if __name__ == '__main__':
+    temperatures = [
+        [9, 3],
+        [9, 0],
+        [11, -3],
+        [11, 1],
+        [8, -3],
+        [7, -3],
+        [-4, -12]
+    ]
+    print(turn(temperatures)) 
+    # {
+    #     'maximum': [9, 9, 11, 11, 8, 7, -4], 
+    #     'minimum': [3, 0, -3, 1, -3, -3, -12]
+    # }
