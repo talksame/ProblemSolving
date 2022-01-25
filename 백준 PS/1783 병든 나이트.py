c67100079ac2dee01e96ed1ce2662738fbf78126
talksame <rnjsdndud4@gmail.com>
2022-01-25 16:02:13 +0900

    0125_2

--- 백준 PS/1783 병든 나이트.py
+++ 백준 PS/1783 병든 나이트.py
@@ -1,15 +1,11 @@
-width, height = map(int, input().split())
-
-if height==1:
-    print(1)
-
-elif height==2:
-    temp=[(width+1)//2,4]
-    print(min(temp))
-
-elif height>=3:
-    if width>=7:
-        print(width-2)
-    else:
-        temp=[4,width]
-        print(min(temp))
+height, width = map(int, input().split())
+cnt = 0
+if height == 1:
+    cnt = 1
+elif height == 2:
+    cnt = min(4, int((width + 1) / 2))
+elif width < 7:
+    cnt = min(width, 4)
+else:
+    cnt = 5 + width - 7
+print(cnt)
